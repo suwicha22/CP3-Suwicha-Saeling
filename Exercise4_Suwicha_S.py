b03637d576574fd1c8f03f79b64c7cad327156b4
suwicha22 <suwicha22@windowslive.com>
2019-09-11 23:53:20 +0700

    Create Exercise4_Suwicha_S.py

---
+++ Exercise4_Suwicha_S.py
@@ -0,0 +1,13 @@
+FE=70
+GB=60
+ICS=50
+CP=81
+print("--- Your Score ---")
+
+print("Foundation English               :",FE)
+
+print("General Business                 :",GB)
+
+print("Introduction to Computer Systems :",ICS)
+
+print("Computer Programming             :",CP)
